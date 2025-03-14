becb58ee50841e9a3b221c8a85a907f547925a5a
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-14 09:21:32 +0100

    refactor: enhance animations and improve code consistency across components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.div className="home container"
+    <motion.div
+      className="home container"
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1}}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
     >
-      <motion.h2 
-        animate={{}}
-      >
-        Welcome to Pizza Joint
-      </motion.h2>
+      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
         <motion.button
-          animate={{}}
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 8px rgb(255,255,255)",
+          }}
         >
           Create Your Pizza
         </motion.button>
       </Link>
     </motion.div>
-  )
-}
+  );
+};
 
 export default Home;
