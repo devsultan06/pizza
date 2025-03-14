c10a96bd30de1fa579432f4eab8145a0ae8db111
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-14 10:17:23 +0100

    refactor: implement motion animations for Base, Order, and Toppings components

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -5,12 +5,34 @@ import { motion } from "framer-motion";
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      x: "100vw",
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { type: "spring", delay: 0.5 },
+    },
+  };
+
+  const nextVariants = {
+    hidden: {
+      x: "-100vw",
+    },
+    visible: {
+      x: 0,
+      transition: { type: "spring", stiffness: 120 },
+    },
+  };
+
   return (
     <motion.div
       className="base container"
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", stiffness: 120 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -32,9 +54,7 @@ const Base = ({ addBase, pizza }) => {
       {pizza.base && (
         <motion.div
           className="next"
-          initial={{ x: "-100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", stiffness: 120 }}
+          variants={nextVariants}
         >
           <Link to="/toppings">
             <motion.button
